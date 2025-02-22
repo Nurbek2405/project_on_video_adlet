aa75c2bcc71a0a9c4fa22e3dbcd3e5378e8d8f10
Nurbek2405 <67856846+Nurbek2405@users.noreply.github.com>
2025-02-22 23:37:20 +0500

    Delete app directory

--- app/apis/depends/user.py
+++
@@ -1,7 +0,0 @@
-from typing import Annotated
-from fastapi import Depends
-
-from sqlalchemy.orm import Session
-
-from app.core.database.helper import get_session
-from app.core models.
--- app/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-
-# entry point
-app = FastAPI()
-
-app.include_router(user_router)
